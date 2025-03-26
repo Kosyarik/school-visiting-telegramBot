59b5b0101c33795532a4c0e6dfd30303a239e82e
YARIK\Yarik <kosyarik@gmai.com>
2025-03-26 20:27:02 +0200

    make webhook

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const { Telegraf, session } = require('telegraf');
 require('dotenv').config();
 const { setupHandlers } = require('./handlers/botHandlers');
+const express = require('express'); 
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.use(session());
 
 setupHandlers(bot);
 
-bot.launch().then(() => {
-  console.log('Bot started');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bot.webhookCallback('/telegram-webhook')); 
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  const webhookUrl = `https://${process.env.RENDER_EXTERNAL_HOSTNAME}/telegram-webhook`;
+  bot.telegram.setWebhook(webhookUrl)
+    .then(() => console.log(`Webhook set to ${webhookUrl}`));
 });
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
